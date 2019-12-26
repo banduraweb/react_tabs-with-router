f1282b6931acec20c947c1f6d2fdb629d7d5830c
bandura <banduraweb@ukr.net>
2019-12-26 04:26:25 +0200

    Updates

---
+++ precache-manifest.4c96745303de92ab203770fa07722b49.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bce57f48ab5fbc7366b81b6f380a15b",
+    "url": "/react_tabs-with-router/index.html"
+  },
+  {
+    "revision": "c026e5c552edbedbbed4",
+    "url": "/react_tabs-with-router/static/css/main.71575edd.chunk.css"
+  },
+  {
+    "revision": "22605248ab2c5fe05c27",
+    "url": "/react_tabs-with-router/static/js/2.f8cadd98.chunk.js"
+  },
+  {
+    "revision": "c026e5c552edbedbbed4",
+    "url": "/react_tabs-with-router/static/js/main.8df1dcf1.chunk.js"
+  },
+  {
+    "revision": "da3ca6fceb301548d22d",
+    "url": "/react_tabs-with-router/static/js/runtime~main.5858125e.js"
+  }
+]);
